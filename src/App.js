930d71bd73d944a7cf876a5a98110ea1e07189bf
Dhanush-Karthik <dhanushkarthikgit@gmail.com>
2023-12-01 00:58:15 +0530

    New intro

--- src/App.js
+++ src/App.js
@@ -8,64 +8,20 @@ import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   const [intro, setIntro] = useState(true);
-  useEffect(() => {
-    var index = 0;
-    var data = [
-      "DHANUSH KARTHIK",
-      "JAVA FULL STACK DEV",
-      "SOFTWARE DEV",
-      "WEB DEV",
-    ];
-
-    var span = document.querySelector("span");
-    var section = document.querySelector("section");
-
-    function init() {
-      let txt = document.createTextNode(data[index]);
-      section.dataset.identity = data[index];
-      span.innerText = txt.textContent;
-      index++;
-    }
-
-    init();
-
-    setInterval(function () {
-      let txt = document.createTextNode(data[index]);
-      section.dataset.identity = data[index];
-      span.innerText = txt.textContent;
-      index++;
-      index = index < data.length ? index++ : 0;
-    }, 4501);
-  }, []);
 
   return (
     <>
       <div className={`${intro ? "intro-wrapper" : "intro-hidden"}`}>
         <div className="intro">
-          <section data-identity="Batman">
-            <blockquote>
-              I AM
-              <span></span>
-            </blockquote>
-          </section>
-          <div class="pen__lines-wrapper">
-            <div class="pen__line"></div>
-            <div class="pen__line"></div>
-            <div class="pen__line"></div>
-            <div class="pen__line"></div>
-            <div class="pen__line"></div>
-            <div class="pen__line"></div>
-            <div class="pen__line"></div>
-            <div class="pen__line"></div>
-            <div class="pen__line"></div>
-            <div class="pen__line"></div>
+          <div class="wrapper">
+            <div class="typing-demo">Hi! This is <span>Dhanush Karthik.</span></div>
+            <div class="skip" onClick={() => setIntro(false)}>
+              VISIT PAGE
+            </div>
           </div>
         </div>
-        <div class="skip" onClick={() => setIntro(false)}>
-          VISIT PAGE
-        </div>
       </div>
-      <div className={`${intro?"intro-hidden":"App"}`}>
+      <div className={`${intro ? "intro-hidden" : "App"}`}>
         <ul class="circles">
           <li></li>
           <li></li>
