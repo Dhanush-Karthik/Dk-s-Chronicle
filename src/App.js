fd17a5542e29c785699ea1c9c3b0791abc9d817c
Dhanush-Karthik <dhanushkarthikgit@gmail.com>
2023-12-01 00:42:01 +0530

    Intro page

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import Content from "./components/content/Content";
 import NavBar from "./components/navbar/NavBar";
@@ -6,30 +7,88 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 function App() {
+  const [intro, setIntro] = useState(true);
+  useEffect(() => {
+    var index = 0;
+    var data = [
+      "DHANUSH KARTHIK",
+      "JAVA FULL STACK DEV",
+      "SOFTWARE DEV",
+      "WEB DEV",
+    ];
+
+    var span = document.querySelector("span");
+    var section = document.querySelector("section");
+
+    function init() {
+      let txt = document.createTextNode(data[index]);
+      section.dataset.identity = data[index];
+      span.innerText = txt.textContent;
+      index++;
+    }
+
+    init();
+
+    setInterval(function () {
+      let txt = document.createTextNode(data[index]);
+      section.dataset.identity = data[index];
+      span.innerText = txt.textContent;
+      index++;
+      index = index < data.length ? index++ : 0;
+    }, 4501);
+  }, []);
+
   return (
-    <div className="App">
-      <ul class="circles">
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-      </ul>
-      <div className="stars-container">
-        <div id='stars'></div>
-        <div id='stars2'></div>
-        <div id='stars3'></div>
+    <>
+      <div className={`${intro ? "intro-wrapper" : "intro-hidden"}`}>
+        <div className="intro">
+          <section data-identity="Batman">
+            <blockquote>
+              I AM
+              <span></span>
+            </blockquote>
+          </section>
+          <div class="pen__lines-wrapper">
+            <div class="pen__line"></div>
+            <div class="pen__line"></div>
+            <div class="pen__line"></div>
+            <div class="pen__line"></div>
+            <div class="pen__line"></div>
+            <div class="pen__line"></div>
+            <div class="pen__line"></div>
+            <div class="pen__line"></div>
+            <div class="pen__line"></div>
+            <div class="pen__line"></div>
+          </div>
+        </div>
+        <div class="skip" onClick={() => setIntro(false)}>
+          VISIT PAGE
+        </div>
       </div>
-      <Profile />
-      <Content />
-      <NavBar />
-      <ToastContainer />
-    </div>
+      <div className={`${intro?"intro-hidden":"App"}`}>
+        <ul class="circles">
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+        <div className="stars-container">
+          <div id="stars"></div>
+          <div id="stars2"></div>
+          <div id="stars3"></div>
+        </div>
+        <Profile />
+        <Content />
+        <NavBar />
+        <ToastContainer />
+      </div>
+    </>
   );
 }
 
