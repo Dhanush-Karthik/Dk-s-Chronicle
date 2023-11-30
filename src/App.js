738f3f092ebf9b2628d9e4ef8257fbbb6104f948
Dhanush-Karthik <dhanushkarthikgit@gmail.com>
2023-11-30 23:29:07 +0530

    Changed background

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
-import './App.css';
-import Content from './components/content/Content';
-import NavBar from './components/navbar/NavBar';
-import Profile from './components/profile/Profile';
+import "./App.css";
+import Content from "./components/content/Content";
+import NavBar from "./components/navbar/NavBar";
+import Profile from "./components/profile/Profile";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
     <div className="App">
+      <ul class="circles">
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+      <div className="stars-container">
+        <div id='stars'></div>
+        <div id='stars2'></div>
+        <div id='stars3'></div>
+      </div>
       <Profile />
       <Content />
       <NavBar />
