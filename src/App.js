7f52246c2ff769132810548ae9ffe10a1eb07c33
Dhanush-Karthik <dhanushkarthikgit@gmail.com>
2023-12-16 18:51:13 +0530

    Updated intro page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import "./App.css";
 import Content from "./components/content/Content";
 import NavBar from "./components/navbar/NavBar";
@@ -14,9 +14,10 @@ function App() {
       <div className={`${intro ? "intro-wrapper" : "intro-hidden"}`}>
         <div className="intro">
           <div class="wrapper">
-            <div class="typing-demo">Hi! This is <span>Dhanush Karthik.</span></div>
+            <div class="typing-demo">Hi! This is <span>Dhanush Karthik.</span> </div>
+            <div class="typing-demo1">Computer <span>Engineer</span> and Technology <span>Enthusiast</span></div>
             <div class="skip" onClick={() => setIntro(false)}>
-              VISIT PAGE
+              Know me
             </div>
           </div>
         </div>
